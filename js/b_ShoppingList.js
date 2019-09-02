67cdbf0cee5fabb8ad1acd51b216d99f767ca65d
cmmd2442 <charles.dziuba@rcn.com>
2019-09-02 18:45:51 -0400

    test

--- js/b_ShoppingList.js
+++ js/b_ShoppingList.js
@@ -10,7 +10,7 @@ const divStyle = {
 class ShoppingList extends React.Component {
   render() {
     return (
-      <div className="ShoppingList">
+      <div style = {divStyle} className="ShoppingList">
         {//<h1>Shopping List for {this.props.name}</h1>
         }
         <h1>Shopping List </h1>

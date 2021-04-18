efa3a36623222105b763a225f9f6a405c3c1724a
cmmd2442 <charles.dziuba@rcn.com>
2021-04-17 23:25:28 -0400

    added navbar

--- js/b1-example-work.js
+++ js/b1-example-work.js
@@ -7,28 +7,6 @@ import M418 from './M418';
 import M417 from './M417';
 
 
-import Navbar from './indexer';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Events from './pages/events';
-import AnnualReport from './pages/annual';
-import Teams from './pages/team';
-import Blogs from './pages/blogs';
-import SignUp from './pages/signup';
-
-
-import {
-Nav,
-NavLink,
-Bars,
-NavMenu,
-NavBtn,
-NavBtnLink,
-} from './NavbarElements';
-
-
-
 
 class ExampleWork extends React.Component {
 
@@ -80,24 +58,6 @@ class ExampleWork extends React.Component {
 
     return (
       <span>
-
-
-	<Router>
-	<Navbar />
-	<Switch>
-		<Route path='/' exact component={Home} />
-		<Route path='/about' component={About} />
-		<Route path='/events' component={Events} />
-		<Route path='/annual' component={AnnualReport} />
-		<Route path='/team' component={Teams} />
-		<Route path='/blogs' component={Blogs} />
-		<Route path='/sign-up' component={SignUp} />
-	</Switch>
-	</Router>
-
-
-
-
       <section className="section section--alignCentered section--description">
 
 
--- js/b1_ewm.js
+++ js/b1_ewm.js
@@ -1,28 +1,22 @@
 import React from 'react';
 
-
-const element = ( <h1> HELLO !!!!! </h1> );
-
-
 class ExampleWorkModal extends React.Component {
   render() {
     let example = this.props.example;
     let modalClass = this.props.open ? 'modal--open' : 'modal--closed';
 
     return (
-      <div  className={"background--skyBlue " + modalClass}>
+      <div className={"background--skyBlue " + modalClass}>
         <span className="color--cloud modal__closeButton"
           onClick={this.props.closeModal}>
           <i className="fa fa-window-close-o"></i>
         </span>
         <img alt={example.image.desc}
              className="modal__image"
-             src={example.image.src} />
+             src={example.image.src}/>
         <div className="color--cloud modal__text">
-
           <h2 className="modal__title">
-{example.title}
-            {/* {example.title} */}
+            {example.title}
           </h2>
           <a className="color--skyBlue modal__link"
              href={example.href}>
@@ -33,10 +27,8 @@ class ExampleWorkModal extends React.Component {
           </p>
         </div>
       </div>
-
     )
   }
 }
 
 export default ExampleWorkModal;
-
